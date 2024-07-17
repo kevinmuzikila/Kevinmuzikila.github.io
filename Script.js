727a6182594b23495c94a990c8d64ac83248f337
Sekopi Kevin Mphofela <121860254+kevinmuzikila@users.noreply.github.com>
2024-07-17 11:31:01 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,3 +3,13 @@ function scrollToSect(sectid) {
     document.getElementById(sectid).scrollIntoView({behavior: 'smooth'});
     console.log('Process complete');
 }
+
+function downloadPDF() {
+    const link = document.createElement('a');
+    link.href = 'doc\CV_2024062006163222.pdf';
+    link.download = 'CV_2024062006163222.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
